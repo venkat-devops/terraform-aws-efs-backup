babea47cda23e42a0becfcc74b160cbd7421e2bf
venkatp227 <venkatp227@gmail.com>
2018-05-16 15:50:49 +0100

    testing

--- outputs.tf
+++ outputs.tf
@@ -6,9 +6,9 @@ output "backups_bucket_name" {
   value = "${aws_s3_bucket.backups.bucket_domain_name}"
 }
 
-output "datapipeline_ids" {
-  value = "${aws_cloudformation_stack.datapipeline.outputs["DataPipelineId"]}"
-}
+# output "datapipeline_ids" {
+#   value = "${aws_cloudformation_stack.datapipeline.outputs["DataPipelineId"]}"
+# }
 
 output "security_group_id" {
   value = "${aws_security_group.datapipeline.id}"
